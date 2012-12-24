a097c7624e91837dfdb5c70e39a5da206837e200
Martin Griffin <martinrgriffin@gmail.com>
2012-12-24 20:39:24 +0000

    Added implementation of entity-entity collision.

--- test/entity.js
+++ test/entity.js
@@ -1,45 +1,41 @@
 function E0() {}
-E0.prototype = Entity;
+Entity.mixin(E0);
 
-function E1() {}
-E1.prototype = Entity;
+function E1() {
+	this.hitE0 = false;
+	this.hitE1 = false;
+}
+Entity.mixin(E1);
+E1.prototype.collisionHandlers = {
+	E0: function () { this.hitE0 = true; },
+	E1: function () { this.hitE1 = true; },
+};
 
 var testEntity = {
-	// TODO: Refactor this into multiple functions.
-	testOnCollision: function () {
-		var test = this;
+	testOnCollide: function () {
+		var self = this;
 
 		var hitE0 = false;
 
 		var e0 = new E0();
+
 		e0.collisionHandlers = {
 			E0: function () { hitE0 = true; },
-			E1: function () { this.assert(false, "e0.onCollide(E1)"); }
+			E1: function () { self.assert(false, "e0.onCollide(E1)"); }
 		};
 
 		e0.onCollide(e0);
 		this.assert(hitE0 === true, "!e0.onCollide(E0)");
 
-
-		hitE0 = false;
-		var hitE1 = false;
-
-		E1.prototype.collisionHandlers = {
-			E0: function () { hitE0 = true; },
-			E1: function () { hitE1 = true; },
-		};
 		var e1 = new E1();
-
 		e1.onCollide(e0);
-		this.assert(hitE0 === true, "!e1.onCollide(E0)");
-		this.assert(hitE1 === false, "e1.onCollide(E1)");
+		this.assert(e1.hitE0 === true, "!e1.onCollide(E0)");
+		this.assert(e1.hitE1 === false, "e1.onCollide(E1)");
 
-		hitE0 = false;
-		hitE1 = false;
-
+		e1 = new E1();
 		e1.onCollide(e1);
-		this.assert(hitE0 === false, "e1.onCollide(E0)");
-		this.assert(hitE1 === true, "!e1.onCollide(E1)");
+		this.assert(e1.hitE0 === false, "e1.onCollide(E0)");
+		this.assert(e1.hitE1 === true, "!e1.onCollide(E1)");
 	}
 };
 
