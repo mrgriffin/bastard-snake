07dfc7c7a8b489ec35ecf7cf80134cf86214f9ff
Martin Griffin <martinrgriffin@gmail.com>
2012-12-25 02:02:07 +0000

    Added tests for actions to be performed as a result of an entity's onX method.

--- test/room.js
+++ test/room.js
@@ -18,12 +18,18 @@ var testRoom = {
 				self.assert(this.updated && that.updated, "onCollide before onUpdate");
 				self.assert(this.collision === undefined, "onCollide called twice");
 				this.collision = that;
+				return [ Room.removeEntity(this), Room.removeEntity(that) ];
 			}
 		};
 
 		var e0 = new E0(0, 0, 1, 0);
 		var e1 = new E0(2, 0, -1, 0);
 		var e2 = new E0(0, 0, 0, 0);
+		var e3 = new E0(0, 0, 0, 0);
+
+		e2.onUpdate = function () {
+			return Room.addEntity(e3);
+		};
 
 		room.add(e0);
 		room.add(e1);
@@ -33,6 +39,10 @@ var testRoom = {
 		this.assert(e0.collision === e1, "e0.collision !== e1");
 		this.assert(e1.collision === e0, "e1.collision !== e0");
 		this.assert(e2.collision === undefined, "e2.collision !== undefined");
+		this.assert(!room.contains(function (entity) { return entity === e0; }), "e0 in room");
+		this.assert(!room.contains(function (entity) { return entity === e1; }), "e1 in room");
+		this.assert(room.contains(function (entity) { return entity === e2; }), "e2 not in room");
+		this.assert(room.contains(function (entity) { return entity === e3; }), "e3 not in room");
 	}
 };
 
