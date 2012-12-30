54ac017f144229f19be6df09402cfe134a8ff218
Martin Griffin <martinrgriffin@gmail.com>
2012-12-30 23:19:06 +0000

    Added test case for a snake crashing into its tail.

--- test/snake.js
+++ test/snake.js
@@ -24,6 +24,17 @@ var testSnake = {
 		this.assert(snake.x === 2 && snake.y === 0, "snake.{x.y} !== {2,0}");
 		this.assert(snake.tail.x === 1 && snake.y === 0, "snake.tail.{x,y} !== {1,0}");
 		this.assert(snake.tail.tail.x === 0 && snake.tail.tail.y === 0, "snake.tail.tail.{x,y} !== {0,0}");
+	}, testCrash: function () {
+		var room = new Room(3, 1);
+
+		var snake = new Snake(0, 0, Direction.RIGHT, 3);
+		room.add(snake);
+
+		room.update();
+		room.update();
+		snake.direction = Direction.LEFT;
+		room.update();
+		this.assert(snake.crashed === true, "snake did not crash");
 	}
 };
 
