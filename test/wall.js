5a145011ca510ac7243fa78d9a12bb3e1547d604
Martin Griffin <martinrgriffin@gmail.com>
2013-01-02 01:42:23 +0000

    Added test case for snake-wall collisions.

---
+++ test/wall.js
@@ -0,0 +1,15 @@
+var testWall = {
+	// TODO: Does this test really belong in wall?
+	testCrash: function () {
+		var snake = new Snake(0, 0, Direction.RIGHT, 1);
+		var wall = new Wall(1, 0);
+		var room = new Room(2, 1);
+		room.add(snake);
+		room.add(wall);
+		room.update();
+
+		this.assert(snake.crashed === true, "!snake.crashed");
+	}
+};
+
+TestRunner.runAll(testWall, print);
