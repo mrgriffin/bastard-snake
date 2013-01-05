4b159a45847d279d64deaa527b269449f68181c9
Martin Griffin <martinrgriffin@gmail.com>
2013-01-05 00:57:54 +0000

    Improved documentation for Wall.

--- www/js/wall.js
+++ www/js/wall.js
@@ -1,20 +1,34 @@
 /*!
  * \file wall.js
- * \brief Wall that prevents other entities from moving into a space.
+ * \brief Wall that occupies a cell.
  */
 
 /*!
  * \class Wall
  * \implements Entity
- * \brief  Wall that prevents other entities from moving into a space.
+ * \brief  Wall that occupies a cell.
  */
 /*!
- * \fn Wall(x, y)
+ * \fn Wall::Wall(int x, int y)
  * \memberof Wall
+ * \public
  * \brief Constructs a wall at \p x, \p y.
  */
 function Wall(x, y) {
+	/*!
+	 * \property int Wall::x
+	 * \memberof Wall
+	 * \protected
+	 * \brief The X coordinate of this wall.
+	 */
 	this.x = x;
+	/*!
+	 * \property int Wall::y
+	 * \memberof Wall
+	 * \protected
+	 * \brief The Y coordinate of this wall.
+	 */
 	this.y = y;
 }
+
 Entity.mixin(Wall);
