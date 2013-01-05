7be501695fe6c5ec8a023d2760777b4aed8f8f78
Martin Griffin <martinrgriffin@gmail.com>
2013-01-05 01:02:04 +0000

    Improved documentation for Portal.

--- www/js/portal.js
+++ www/js/portal.js
@@ -10,19 +10,45 @@
  */
 var Portal = (function () {
 	function Portal(x, y) {
+		/*!
+		 * \property int Portal::x
+		 * \memberof Portal
+		 * \protected
+		 * \brief The X coordinate of this portal.
+		 */
 		this.x = x;
+		/*!
+		 * \property int Portal::y
+		 * \memberof Portal
+		 * \protected
+		 * \brief The Y coordinate of this portal.
+		 */
 		this.y = y;
+		/*!
+		 * \property Portal Portal::pair
+		 * \memberof Portal
+		 * \private
+		 * \brief The portal that this portal teleports entities to.
+		 */
 	}
 	Entity.mixin(Portal);
+
 	Portal.prototype.onCollide = {
+		/*!
+		 * \fn Action | Action[] Portal::onCollide(Entity else)
+		 * \memberof Portal
+		 * \protected
+		 * \brief Teleports \p else to the position of \c pair.
+		 */
 		else: function (that) {
 			return new Entity.MoveAction(that, this.pair.x, this.pair.y);
 		}
 	};	
 
 	/*!
-	 * \fn Portal[] makePortals(x1, y1, x2, y2)
+	 * \fn Portal[] Portal::makePortals(x1, y1, x2, y2)
 	 * \memberof Portal
+	 * \public
 	 * \brief Returns an array of two connected portals at \p x1, \p y1 and \p x2, \p y2.
 	 */
 	function makePortals(x1, y1, x2, y2) {
